bd5a22baf1311b7359a84c5549b5000dec581906
Aditya Uniyal <47002387+alekady7@users.noreply.github.com>
2023-01-22 23:04:41 +0530

    created profile page form

--- src/components/RightMenu/Profile/Profile.js
+++ src/components/RightMenu/Profile/Profile.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Profile() {
+  const [formValue, setformValue] = useState({
+    username: '',
+    password: '',
+  })
   return (
-    <div>
-        Profile
+    <div className='profile'>
+      <div className='form'>
+        <form>
+          <label>
+            Name
+            <input type='text' name='username' value={formValue.username} placeholder='Your name' />
+          </label>
+          <br />
+          <label>
+            Password
+            <input type='password' name='password' value={formValue.password} placeholder='Enter password' />
+          </label>
+        </form>
+      </div>
     </div>
   )
 }
