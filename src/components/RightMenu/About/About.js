ab8162fed090fad95f171fccd58d51ba3a5c1bbc
Aditya Uniyal <47002387+alekady7@users.noreply.github.com>
2023-01-22 10:57:06 +0530

    updated about page

--- src/components/RightMenu/About/About.js
+++ src/components/RightMenu/About/About.js
@@ -5,22 +5,24 @@ import './About.css'
 function About() {
     return (
         <div className='about'>
-            &nbsp;
-            <h1>About us</h1>
-            <br /><br />
-            <p>
-                -Pro Network is a platform where people can create professional networks, talk with various professionals by communicating, sharing knowledge, guide people, get guided by people and so on.
-            </p>
-            <br />
-            <p>
-                -At Pro Network, we have designed a system that works well for both students and professionals, and streamlined the networking/mentoring process making it easy and efficient.
-            </p>
-            <br />
-            <p>
-                -Join the amazing community of immense professinals and start helping/benefitting from awesome posts!
-            </p>
-            <br />            
-            <Link to='/' className='home-btn'>Go to Home</Link>
+            <div className='about__us'>
+                &nbsp;
+                <h1>About us</h1>
+                <br /><br />
+                <p>
+                    -Pro Network is a platform where people can create professional networks, talk with various professionals by communicating, sharing knowledge, guide people, get guided by people and so on.
+                </p>
+                <br />
+                <p>
+                    -At Pro Network, we have designed a system that works well for both students and professionals, and streamlined the networking/mentoring process making it easy and efficient.
+                </p>
+                <br />
+                <p>
+                    -Join the amazing community of immense professinals and start helping/benefitting from awesome posts!
+                </p>
+                <br />
+                <Link to='/' className='home-btn'>Go to Home</Link>
+            </div>
         </div>
     )
 }
